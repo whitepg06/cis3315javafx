1ed0ec7924ef6bf56a1ada05e519a614d1e74f73
Prof Phillips <profphillips@gmail.com>
2017-03-07 14:13:21 +0000

    Removed extra imports from c15n01

--- src/week7/C15N01.java
+++ src/week7/C15N01.java
@@ -3,8 +3,6 @@ package week7;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -12,7 +10,6 @@ import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
